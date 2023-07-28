106a4d0dd09e8c52354ffa082c6310d059787779
RobertoCarrera <robertocarrera93@gmail.com>
2023-07-28 12:46:24 +0200

    movido bien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from '../about/about.component';
-import { CharactersComponent } from '../characters/characters.component';
-import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CharactersComponent } from './characters/characters.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AppRoutingModule } from '../menu/app-routing.module';
+import { AppRoutingModule } from '../app-routing.module';
 import { AppComponent } from '../app.component';
 import { CharactersComponent } from '../characters/characters.component';
 import { AboutComponent } from '../about/about.component';
